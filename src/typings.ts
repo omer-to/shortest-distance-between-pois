ce29a73eda86e9203f595a5aa7c1306b7c141b20
Omer Toraman <omer.toraman@akinon.com>
2021-08-07 17:39:28 +0300

    Add interface for Pointthat denotes a single location

---
+++ src/typings.ts
@@ -0,0 +1,13 @@
+export interface Point {
+      /**
+       * In Decimal Degrees
+       * Must be between -90 and 90, both inclusive
+       */
+      latitude: number
+
+      /**
+       * In Decimal Degrees
+       * Must be between -180, and 180, both inclusive
+       */
+      longitude: number
+}
