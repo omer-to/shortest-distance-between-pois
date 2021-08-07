98c3a6379e570c6e785b13cf0be703ed2a5d3fde
Omer Toraman <omer.toraman@akinon.com>
2021-08-07 21:52:19 +0300

    Add unit tests for decimalDegreesToRadians

---
+++ tests/decimalDegreesToRadians.test.ts
@@ -0,0 +1,35 @@
+import { decimalDegreesToRadians } from '../src/decimalDegreesToRadians'
+
+describe('Test Case for decimalDegreesToRadians function', () => {
+      test('Positive decimal degrees should result in positive radians', () => {
+            const degree = 1
+            const radians = decimalDegreesToRadians(degree)
+            expect(radians).toBeGreaterThan(0)
+      })
+
+      test('Negative decimal degrees should result in positive radians', () => {
+            const degree = -1
+            const radians = decimalDegreesToRadians(degree)
+            expect(radians).toBeLessThan(0)
+      })
+
+      test('Zero decimal degrees should result in zero radians', () => {
+            const degree = 0
+            const radians = decimalDegreesToRadians(degree)
+            expect(radians).toBe(0)
+      })
+
+      test('Opposite signs of the same degree should result in the same radians with opposite sign', () => {
+            const degree = 15.1
+
+            const
+                  positiveDegree = degree,
+                  negativeDegree = -degree
+
+            const
+                  positiveRadians = decimalDegreesToRadians(positiveDegree),
+                  negativeRadians = decimalDegreesToRadians(negativeDegree)
+
+            expect(positiveRadians).toBe(Math.abs(negativeRadians))
+      })
+})
