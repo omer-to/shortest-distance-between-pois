a8066b6e814af06e5d9d6861e062af3495d0bc2f
Omer Toraman <omer.toraman@akinon.com>
2021-08-07 18:26:31 +0300

    Find all possible routes, and calculate the shortest by using permutation

---
+++ src/findShortestAmongAllRoutes.ts
@@ -0,0 +1,30 @@
+import { HaversineCalculator } from './HaversineCalculator'
+import { getPermutations } from './getPermutations'
+
+import type { Poi, DistanceCalculator } from './typings'
+
+export function findShortestAmongAllRoutes(pois: Poi[], sourceIndex: number, calculateDistance: DistanceCalculator = HaversineCalculator.distanceBetween) {
+      const arr = pois.slice(0, sourceIndex).concat(pois.slice(sourceIndex + 1)) // Exclude the starting point and make a shallow copy
+      const start = pois[sourceIndex]
+
+      const [permutations, distances] = getPermutations(arr, start, calculateDistance)
+
+      let shortestDistance = Infinity
+      let shortestRouteIndex = 0
+      const lenDistances = distances.length
+      // Math.min throws a RangeError for exceeding maximum call stack size.
+      for (let index = 0; index < lenDistances; index++) {
+            const distance = distances[index]
+            if (distance < shortestDistance) {
+                  shortestDistance = distance
+                  shortestRouteIndex = index
+            }
+      }
+      const shortestRoute = permutations[shortestRouteIndex]
+
+      return {
+            shortestRoute,
+            shortestDistance
+      }
+}
+
