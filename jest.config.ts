c07e0c3c32259d3a63e783112f2695fd175d08be
Omer Toraman <omer.toraman@akinon.com>
2021-08-07 20:47:32 +0300

    Add config file for jest

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+      preset: 'ts-jest',
+      verbose: true,
+      testEnvironment: 'node'
+};
+
+export default config;
