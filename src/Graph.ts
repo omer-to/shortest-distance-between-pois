047f123fdef58ee91ef3390da075a0350166e2a7
Omer Toraman <omer.toraman@akinon.com>
2021-08-07 18:08:33 +0300

    Implement constructor

--- src/Graph.ts
+++ src/Graph.ts
@@ -1,7 +1,18 @@
 import { HaversineCalculator } from './HaversineCalculator'
 
-import type { Poi } from './typings'
+import type { Poi, Point, DistanceCalculator } from './typings'
+
 
 export class Graph {
-
+      private nodes: Poi[]
+      private visited: Poi[]
+      private unvisited: Poi[]
+      private calculateDistance: DistanceCalculator
+      constructor(pois: Poi[], sourceIndex: number, calculateDistance: DistanceCalculator) {
+            // Exclude the starting Poi and shallow copy
+            this.nodes = pois.slice(0, sourceIndex).concat(pois.slice(sourceIndex + 1))
+            this.visited = [pois[sourceIndex]]
+            this.unvisited = this.nodes
+            this.calculateDistance = calculateDistance || HaversineCalculator.distanceBetween
+      }
 }
