4eb55c6ea19572c4586323c6de0ae2b199da42c2
Omer Toraman <omer.toraman@akinon.com>
2021-08-08 21:16:56 +0300

    Add JSDoc for the parameters, and description

--- src/outputResult.ts
+++ src/outputResult.ts
@@ -5,8 +5,13 @@ import { ensureOutputDirectoryExists, outputDir } from './ensureOutputDirectoryE
 import type { Poi } from './typings'
 
 
-
-
+/**
+ * @description Logs the route and distance, and writes into a file.
+ * 
+ * @param functionName The name of the function used for finding the shortest path
+ * @param route The route that the function finds as the shortest
+ * @param distance The total distance for the route
+ */
 export async function outputResult(functionName: string, route: Poi[], distance: number) {
 
       console.log(`The shortest route using ${functionName} function to find to the shortest path and total distance:`)
